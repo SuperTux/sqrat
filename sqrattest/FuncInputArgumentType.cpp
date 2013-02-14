1428a66f0d19b97abfc4f09d468aaade2286a676
Andy Tai <atai@atai.org>
2013-02-14 17:43:16 +0000

    a unit test for function argument check: note this fails currently.  From a message in the Squirrel forum http://forum.squirrel-lang.org/default.aspx?g=posts&t=2518

---
+++ sqrattest/FuncInputArgumentType.cpp
@@ -0,0 +1,101 @@
+//
+// Copyright (c) 2013 Li-Cheng (Andy) Tai
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you must not
+//  claim that you wrote the original software. If you use this software
+//  in a product, an acknowledgment in the product documentation would be
+//  appreciated but is not required.
+//
+//  2. Altered source versions must be plainly marked as such, and must not be
+//  misrepresented as being the original software.
+//
+//  3. This notice may not be removed or altered from any source
+//  distribution.
+//
+
+#include <gtest/gtest.h>
+#include <sqrat.h>
+#include "Fixture.h"
+
+
+using namespace Sqrat;
+
+class Vector2
+{
+public:
+    float value;
+    
+    //some Vector Math overloads and members here
+    operator float() const 
+    {
+        return 1.0f; /* test only */    
+    }
+    
+    
+    Vector2 operator +(const Vector2& v) 
+    {
+        value += v.value;
+        return v; /* test only */    
+    }
+    
+    inline float add(const Vector2& v) 
+    {
+        return (*this) + v; //it crashes right here as it references to an nonexistent obj
+    }
+
+};
+
+static const char *sq_code = "\
+    local v = Vector2();\
+    local v2 = Vector2();\
+    v.add(v2); /*good*/  \
+    print(\"1\");\
+    v.add(10); /*crash*/  \
+    print(\"2\");\
+    v.add(); /*crash*/  \
+    print(\"3\");\
+    v.add(\"text\"); /*crash*/  \
+    print(\"4\");\
+       ";
+
+static const char *sq_code0 = "\
+    local v = Vector2();\
+    local v2 = Vector2();\
+    v.add(v2); /*good*/  \
+    print(\"1\");\
+    v.add(10); /*crash*/  \
+       ";
+
+
+
+TEST_F(SqratTest, FuncInputArgumentType) {
+    DefaultVM::Set(vm);
+    
+    Sqrat::Class<Vector2> classVector2(vm);
+    
+    classVector2.Func("add", &Vector2::add);
+    
+    Sqrat::RootTable(vm).Bind("Vector2", classVector2);
+            
+    Script script;
+    try {
+        script.CompileString(_SC(sq_code0));
+    } catch(Exception ex) {
+        FAIL() << _SC("Compile Failed: ") << ex.Message();
+    }
+
+    try {
+        script.Run();
+    } catch(Exception ex) {
+        FAIL() << _SC("Run Failed: ") << ex.Message();
+    }
+
+}
