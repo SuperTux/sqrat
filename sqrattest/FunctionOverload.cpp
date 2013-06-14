c8bbd8d527ffee86f0953a4e1462f1e37a6de976
Andy Tai <atai@atai.org>
2013-06-14 22:05:54 +0000

    Sqrat Sourceforge Bug 29, implementation of support for global functions returning references, with a simple unit test

--- sqrattest/FunctionOverload.cpp
+++ sqrattest/FunctionOverload.cpp
@@ -170,3 +170,129 @@ TEST_F(SqratTest, ConstOverloadTest) {
     }
 }
 
+
+
+class B 
+{
+private:
+    int value;
+public:
+    B(): value(-1) {}
+    
+    int set(int v)
+    {
+        value = v;
+    }
+    int get()
+    {
+        //std::cout << "B's address is " << (long) this << std::endl;
+        return value;
+    }
+    
+    static string shared;
+    static int sharedInt;
+};
+
+    
+static B& getB(B &b)
+{
+    return b;
+}
+
+static B& getB2(B *b, int, char *)
+{
+    return *b;
+}
+
+static B& getB4( B & b, B *, const B, int)
+{
+    return b;
+}
+
+static B* getBPtr(B *b)
+{
+    return b;
+}
+
+string B::shared ;
+int B::sharedInt = -1;
+
+TEST_F(SqratTest, FunctionReturningReferencesToClassesWithStaticMembers) {
+    DefaultVM::Set(vm);
+
+    Class<B> _B;
+    _B
+    .Func("set", &B::set)
+    .Func("get", &B::get)
+    .StaticVar("shared", &B::shared)
+    .StaticVar("sharedInt", &B::sharedInt);
+    
+    RootTable().Bind("B", _B);
+    RootTable().Func("getB", &getB);
+    RootTable().Func("getB2", &getB2);
+    RootTable().Func("getB4", &getB4);
+    RootTable().Func("getBPtr", &getBPtr);
+    
+    Script script;
+    try {
+        script.CompileString(_SC(" \
+            b <- B();\
+            bb <- B(); \
+            \
+			gTest.EXPECT_INT_EQ(b.get(), -1); \
+			gTest.EXPECT_INT_EQ(bb.sharedInt, -1); \
+			gTest.EXPECT_INT_EQ(b.sharedInt, -1); \
+            b.set(12);\
+            b.shared = \"a long string\"; \
+            b.sharedInt = 1234; \
+            gTest.EXPECT_STR_EQ(bb.shared, \"a long string\"); \
+            gTest.EXPECT_STR_EQ(b.shared, \"a long string\"); \
+			gTest.EXPECT_INT_EQ(bb.sharedInt, 1234); \
+			gTest.EXPECT_INT_EQ(b.get(), 12); \
+			local b1 = getBPtr(b);\
+            b.set(20);\
+			gTest.EXPECT_INT_EQ(b1.get(), 20); \
+			local b2 = getB(b);\
+            b.set(40);\
+			gTest.EXPECT_INT_EQ(b1.get(), 40); \
+			gTest.EXPECT_INT_EQ(b2.get(), 40); \
+			local b3 = getB2(b, 12, \"test\");\
+            b.set(60);\
+			gTest.EXPECT_INT_EQ(b2.get(), 60); \
+			gTest.EXPECT_INT_EQ(b3.get(), 60); \
+			local b4 = getB4(b, b, b, 33);\
+            b.set(80);\
+			gTest.EXPECT_INT_EQ(b3.get(), 80); \
+			gTest.EXPECT_INT_EQ(b4.get(), 80); \
+            \
+            bb.shared = \"short str\"; \
+            gTest.EXPECT_STR_EQ(b2.shared, \"short str\"); \
+            gTest.EXPECT_STR_EQ(b3.shared, \"short str\"); \
+            gTest.EXPECT_STR_EQ(b.shared, \"short str\"); \
+            gTest.EXPECT_STR_EQ(b1.shared, \"short str\"); \
+            gTest.EXPECT_STR_EQ(b4.shared, \"short str\"); \
+			gTest.EXPECT_INT_EQ(b.sharedInt, 1234); \
+			gTest.EXPECT_INT_EQ(b1.sharedInt, 1234); \
+			gTest.EXPECT_INT_EQ(b2.sharedInt, 1234); \
+			gTest.EXPECT_INT_EQ(b3.sharedInt, 1234); \
+			gTest.EXPECT_INT_EQ(b4.sharedInt, 1234); \
+            b4.shared = \"abcde\"; \
+            b4.sharedInt = 9999; \
+            gTest.EXPECT_STR_EQ(bb.shared, \"abcde\"); \
+			gTest.EXPECT_INT_EQ(bb.sharedInt, 9999); \
+			gTest.EXPECT_INT_EQ(b1.sharedInt, 9999); \
+            "));
+    }
+    catch (Sqrat::Exception ex) {
+        FAIL() << _SC("Compile Failed: ") << ex.Message();
+    }
+
+    try {
+        script.Run();
+    }
+    catch (Exception ex) {
+        FAIL() << _SC("Run Failed: ") << ex.Message();
+    }
+    
+}
+
