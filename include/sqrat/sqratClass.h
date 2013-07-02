f1cd9a2c80cf07451d69b2c76e4d4cabb4acba40
Andy Tai <atai@atai.org>
2013-07-02 00:16:45 +0000

    patch from ZackTheHuman in the Squirrel forum, mixed const/non-const getters and setters

--- include/sqrat/sqratClass.h
+++ include/sqrat/sqratClass.h
@@ -184,6 +184,22 @@ public:
 
         return *this;
     }
+    
+    /// Bind a class property (variable accessed via a setter and getter)
+    template<class V>
+    Class& Prop(const SQChar* name, V (C::*getMethod)() const, void (C::*setMethod)(V)) {
+        if(getMethod != NULL) {
+            // Add the getter
+            BindAccessor(name, &getMethod, sizeof(getMethod), SqMemberOverloadedFunc(getMethod), ClassType<C>::GetTable(vm));
+        }
+
+        if(setMethod != NULL) {
+            // Add the setter
+            BindAccessor(name, &setMethod, sizeof(setMethod), SqMemberOverloadedFunc(setMethod), ClassType<C>::SetTable(vm));
+        }
+
+        return *this;
+    }
 
     /// Bind a class property (variable accessed via a setter and getter)
     template<class V>
